0ecc63c460c48e8b09f9204cc78c92621c5d9847
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-16 20:56:02 +0530

    converted MainViewModel.java to Kotlin file MainViewModel.kt

---
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/ui/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.mindorks.bootcamp.learndagger.ui
+
+import com.mindorks.bootcamp.learndagger.data.local.DatabaseService
+import com.mindorks.bootcamp.learndagger.data.remote.NetworkService
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(val databaseService: DatabaseService, val networkService: NetworkService) {
+
+    fun getSomeData():String = "${databaseService.getDummyData()} ${networkService.getDummyData()}"
+
+}
